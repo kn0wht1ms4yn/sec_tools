66d5a8b5103e292f39786cfec02695f3da3a7ab7
kn0wht1ms4yn <knowhtimsayn@mail.com>
2025-07-04 16:24:21 -0400

    added exfil server

---
+++ server/exfil/README.md
@@ -0,0 +1,4 @@
+
+- a simple web app that recvs a request and shows it in a `<textarea>`
+- to log a request send it to `/exfil`
+
---
+++ server/exfil/backend/server.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, send_from_directory
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from os import path
+import json
+import time
+
+app = Flask(__name__)
+app.config['SECRET_LEY'] = 'FIXME: CHANGE_THIS_FOR_PROD'
+socketio = SocketIO(app)
+
+RELAY_ON = False
+
+STATIC_DIR = path.join(app.root_path, '../frontend')
+
+''' --- Flask --- '''
+@app.route('/')
+def index():
+    return send_from_directory(path.join(STATIC_DIR), 'index.html')
+
+@app.route('/exfil')
+def exfil():
+    # headers
+    headers = ''
+    for header in request.headers:
+        headers += f'<span class="text-warning">{header[0]}</span>: {header[1]}<br />'
+    
+    # get data
+    get_data = ''
+    if len(request.args):
+        get_data += '?'
+        data = map(lambda x: f'{x}={request.args[x]}', request.args)
+        data = '&'.join(data)
+        get_data += data
+
+    # body data
+    body_data = request.data.decode()
+
+    # construct response
+    path = f'<span class="text-info">{request.method} {request.path}{get_data} {request.environ.get("SERVER_PROTOCOL")}</span>'
+    resp = f'{path}<br />'
+    resp += f'{headers}<br />'
+    resp += f'{body_data}'
+
+    socketio.send({ 'req': resp })
+    return 'OK'
+
+''' --- socketIO --- '''
+@socketio.on('connect')
+def sock_msg(msg):
+    print(f'connect recvd: {msg}')
+
+socketio.run(app, host='0.0.0.0', port=8888, debug=True)
